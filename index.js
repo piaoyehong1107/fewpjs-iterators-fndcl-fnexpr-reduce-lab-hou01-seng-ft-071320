0ade507c80e6f795b9cc131da285823bc5bbf814
Yehong Piao <piaoyehong1107@gmail.com>
2020-09-05 23:30:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
  let total=0;
 function totalBatteries(arr){
-  arr.reduce((ele)=>{
+  let total=arr.reduce((ele)=>{
    return total+=ele;
   },0)
   return total;
